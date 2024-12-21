02521b4712a06eea1e9394ba7e42e7cff37e225a
G0dfathera <108694995+G0dfathera@users.noreply.github.com>
2024-12-21 20:33:45 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Install nmap
-apt install -y nmap
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
 
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
