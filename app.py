89beba9d72070b39d558de799db7cfac425c891e
G0dfathera <108694995+G0dfathera@users.noreply.github.com>
2024-12-23 16:38:33 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from db import app
+from db import app, db
 
 # Run the app
 if __name__ == "__main__":
-
+    # with app.app_context():
+    #     db.create_all()
     from routes import *
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
