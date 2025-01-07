3ac947a8d8b6a15cfa46681c9c96f96b153ea637
G0dfathera <108694995+G0dfathera@users.noreply.github.com>
2025-01-07 16:27:50 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,14 @@ import os
 
 load_dotenv()  # Load environment variables from the .env file
 
-# Initialize the Flask app
 app = Flask(__name__)
 
-# Secret key for session management, stored in the .env file
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecretkey')  # Default key for development
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecretkey')
 
-# Configure the database URI (use SQLite or PostgreSQL depending on environment)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///users.db')  # Default SQLite URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy modification tracking
 
-# Initialize the database
 db = SQLAlchemy(app)
 
-# Initialize Flask-Login
 login_manager = LoginManager(app)
 login_manager.login_view = "login"  # Redirect to login page if not authenticated
